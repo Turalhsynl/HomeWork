bf9cbc541d18ad1240d3bd97e116d10c544d2537
Tural.Dev <tural.huseynli.2005@gmail.com>
2025-01-26 00:47:33 +0400

    finish

---
+++ UserInfo/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using UserInfo.Entities;
+
+namespace UserInfo.Controllers
+{
+    public class UsersController : Controller
+    {
+        public static List<User> users = new List<User>()
+    {
+        new User
+        {
+            Id = 1,
+            Name = "Tural",
+            Surname = "Huseynli",
+            Image = "image",
+            Age = 20
+        },
+        new User
+        {
+            Id = 2,
+            Name = "Adolf",
+            Surname = "Hitler",
+            Image = "image",
+            Age = 1945
+        }
+    };
+
+        public ViewResult Index()
+        {
+            return View(users);
+        }
+
+        public ViewResult Details(int id)
+        {
+            User user = users.Find(x => x.Id == id);
+            return View(user);
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            user.Id = users.Count > 0 ? users[^1].Id + 1 : 1;
+            users.Add(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ UserInfo/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserInfo.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Image { get; set; }
+        public int Age { get; set; }
+    }
+}
